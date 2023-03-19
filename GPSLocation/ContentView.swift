47576e78fb60b237d0563ebf013327f5c8b31a7e
Matt <matt.cavallo56@gmail.com>
2023-03-18 21:01:17 -0500

    Initial Commit

---
+++ GPSLocation/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  GPSLocation
+//
+//  Created by Matthew Cavallo on 3/18/23.
+//
+
+import Combine
+import SwiftUI
+
+struct ContentView: View {
+    
+    @StateObject var deviceLocationService = DeviceLocationService.shared
+    
+    @State var tokens: Set<AnyCancellable> = []
+    @State var coordinates: (lat: Double, lon: Double) = (0,0)
+    
+    var body: some View {
+        VStack {
+            Text("Latitude: \(coordinates.lat)")
+                .font(.largeTitle)
+            Text("Longitude: \(coordinates.lon)")
+                .font(.largeTitle)
+        }
+        .onAppear {
+            observeCoordinateUpdates()
+            observeDeniedLocationAccess()
+        }
+    }
+    
+    func observeCoordinateUpdates() {
+        deviceLocationService.coordinatesPublisher
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                print("Handle \(completion) for error and finished subscription.")
+            } receiveValue: { coordinates in
+                self.coordinates = (coordinates.latitude, coordinates.longitude)
+            }
+            .store(in: &tokens)
+    }
+    
+    func observeDeniedLocationAccess() {
+        deviceLocationService.deniedLocationAccessPublisher
+            .receive(on: DispatchQueue.main)
+            .sink {
+                print("Handle access denied event, possibly with an alert.")
+            }
+            .store(in: &tokens)
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
---
+++ GPSLocation/GPSLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPSLocationApp.swift
+//  GPSLocation
+//
+//  Created by Matthew Cavallo on 3/18/23.
+//
+
+import SwiftUI
+
+@main
+struct GPSLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
